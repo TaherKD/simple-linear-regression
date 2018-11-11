e57e559ca2c91668ce26c5b99ea40e997bace9a5
Taher Dhilawala <taher.dhilawala@yahoo.com>
2018-11-11 10:03:01 +0530

    Simple Linear Regression using Python

---
+++ Python/simple_linear_regression_T.py
@@ -0,0 +1,39 @@
+#SIMPLE LINEAR REGRESSION
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+#FITTING SIMPLE LINEAR REGRESSION TO TRAINING SET
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#PREDICTING THE TEST SET RESULTS
+y_pred = regressor.predict(X_test)
+
+#VISUALISING THE TRAINING SET RESULTS
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#VISUALISING THE TESTING SET RESULTS
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Testing Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
